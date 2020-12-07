f6dfd35a078127f1a7225c9d50776274022e16f0
lim <569212066@qq.com>
2020-12-07 16:11:37 +0800

    feat: add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import React from "react";
+import { Route, Link, Switch, BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li><Link to='/'>Home</Link></li>
+            <li><Link to='/about'>About</Link></li>
+            <li><Link to='/user'>User</Link></li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/about">这里是About</Route>
+          <Route path="/user">这里是User</Route>
+          <Route path="/" component={Home}></Route>
+        </Switch>
+      </div>
+    </Router>
   );
+}
+
+function Home() {
+  return (
+    <h2>Home</h2>
+  )
 }
 
 export default App;
