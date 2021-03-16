77730337f06c5b0c79bfc53da56c2a12255a1edb
lim <569212066@qq.com>
2021-03-16 14:55:24 +0800

    feat: useCallback

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 // import logo from './logo.svg';
 // import './App.css';
-import React from "react";
+import React, {useState, useEffect, useMemo, useCallback } from "react";
+import Button from './pages/Button'
 import { Route, Link, Switch, BrowserRouter as Router } from "react-router-dom";
 
 function App() {
+  const [count1, setCount1] = useState(0);
+  const [count2, setCount2] = useState(0);
+  const [count3, setCount3] = useState(0);
+  const handleClickButton1 = () => {
+    setCount1(count1 + 1)
+  }
+  const handleClickButton2 = useCallback(()=> {
+    setCount2(count2 + 1)
+  }, [count2])
   return (
     <Router>
-      <div>
+      {/* <div>
         <nav>
           <ul>
             <li><Link to='/'>Home</Link></li>
@@ -20,6 +30,15 @@ function App() {
           <Route path="/user">这里是User</Route>
           <Route path="/" component={Home}></Route>
         </Switch>
+      </div> */}
+      <div>
+        <Button onClickButton={handleClickButton1}>Button1</Button>
+      </div>
+      <div>
+        <Button onClickButton={handleClickButton2}>Button2</Button>
+      </div>
+      <div>
+        <Button onClickButton={()=> setCount3(count3 + 1)}>Button3</Button>
       </div>
     </Router>
   );
---
+++ src/pages/Button.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Button({onClickButton, children}) {
+  return (
+    <>
+      <button onClick={onClickButton}>{children}</button>
+      <span>{Math.random()}</span>
+    </>
+  )
+}
+export default React.memo(Button)
