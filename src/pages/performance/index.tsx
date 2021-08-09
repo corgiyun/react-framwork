e55452975925b7bc1bc3f54bd0f83661c7b0eedb
lim <569212066@qq.com>
2021-08-09 17:11:20 +0800

    feat: test

---
+++ src/pages/performance/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import style from './style.module.less'
+
+const Index = () => {
+  return (
+    <>
+      <div className={style.scrollArea}>
+        <div className={style.scroll}>
+          <span className={style.scrollContent}>这是一个张浩然</span>
+        </div>
+      </div>
+    </>
+  )
+}
+export default Index
--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -1,6 +1,6 @@
 import AsyncComponent from '../components/hoc/AsyncComponent'
 
-const Test = AsyncComponent(() => import('../pages/performance'))
+const Test = AsyncComponent(() => import('../pages/performance/index'))
 
 const routes = [{ path: '/test', component: Test }]
 
