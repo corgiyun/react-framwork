c6930e4f1209e60ec136a2a68d5aa749fb79a011
lim <569212066@qq.com>
2021-09-03 14:22:31 +0800

    test

--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -1,7 +1,11 @@
 import AsyncComponent from '../components/hoc/AsyncComponent'
 
 const Test = AsyncComponent(() => import('../pages/performance/index'))
+const XiaoHe = AsyncComponent(() => import('../pages/xiaohe/index'))
 
-const routes = [{ path: '/test', component: Test }]
+const routes = [
+  { path: '/test', component: Test },
+  { path: '/xiaohe', component: XiaoHe },
+]
 
 export default routes
