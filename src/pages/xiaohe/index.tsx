624ce554c11e54c9a82d93ecc56fad8c276cc685
lim <569212066@qq.com>
2021-11-10 14:25:29 +0800

    feat: add npm-test

--- src/pages/xiaohe/index.tsx
+++ src/pages/xiaohe/index.tsx
@@ -1,9 +1,14 @@
 import React, { useEffect, useState, useMemo } from 'react'
+import { a as a1, b as b1 } from 'npm-test'
 import Button from '../../components/Button'
+
+
 
 export default function Index() {
   const [a, setA] = useState(0)
   const [b, setB] = useState(1)
+  console.log(a1, b1);
+
   const computed = (num) => {
     console.log('computed');
     return num + 1
